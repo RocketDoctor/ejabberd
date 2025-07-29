517cbd787a9a10d0d886528bbe5577799bf6d4a9
weirongzhang <weirongzhang2016@gmail.com>
2025-07-29 11:17:14 -0400

    index on identifier_timestamp

--- sql/pg.new.sql
+++ sql/pg.new.sql
@@ -253,7 +253,7 @@ CREATE INDEX i_spool_sh_username ON spool USING btree (server_host, username);
 
 CREATE TABLE archive (
     username text NOT NULL,
-    server_host text NOT NULL,
+    server_host text NOT NULL default '',
     identifier varchar(64),
     timestamp BIGINT NOT NULL,
     peer text NOT NULL,
@@ -268,7 +268,7 @@ CREATE TABLE archive (
 );
 
 CREATE INDEX i_archive_sh_username_timestamp ON archive USING btree (server_host, username, timestamp);
-CREATE INDEX i_archive_sh_identifier_timestamp ON archive USING btree (identifier, server_host, username, timestamp);
+CREATE INDEX i_archive_identifier_timestamp ON archive USING btree (identifier, timestamp);
 CREATE INDEX i_archive_sh_username_peer ON archive USING btree (server_host, username, peer);
 CREATE INDEX i_archive_sh_username_bare_peer ON archive USING btree (server_host, username, bare_peer);
 CREATE INDEX i_archive_sh_timestamp ON archive USING btree (server_host, timestamp);
